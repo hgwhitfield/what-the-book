b1e85d2719f9b4dd79562a8b2aad96f62f578f4a
Henry Whitfield <harrywhitfield18@gmail.com>
2024-05-26 18:29:02 +0000

    Add DisplayTBRList and addToFavourite functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,3 +44,41 @@ function displayResults(data) {
         resultsSection.appendChild(bookElement);
     });
 }
+
+function addToFavorites(title, author, image) {
+    let tbrList = JSON.parse(localStorage.getItem('tbrList')) || [];
+    if (tbrList.some(book => book.title === title)) {
+        alert('This book is already in your TBR list.');
+        return;
+    }
+
+    tbrList.push({ title, author, image });
+    localStorage.setItem('tbrList', JSON.stringify(tbrList));
+    displayTBRList();
+}
+
+function displayTBRList() {
+    const tbrList = JSON.parse(localStorage.getItem('tbrList')) || [];
+    const tbrSection = document.getElementById('tbr-list');
+    tbrSection.innerHTML = '';
+
+    tbrList.forEach(book => {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'col-xs-12', 'tbr-item'); // Responsive classes
+        bookElement.innerHTML = `
+            <div class="card h-100">
+                <figure>
+                    <img src="${book.image}" class="card-img-top" alt="${book.title} cover image">
+                    <figcaption>${book.title} by ${book.author}</figcaption>
+                </figure>
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">${book.author}</p>
+                </div>
+            </div>
+        `;
+        tbrSection.appendChild(bookElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', displayTBRList);
